756c80e37e7e4b391870db13a093c1e18add3d5a
mikord45 <51155742+mikord45@users.noreply.github.com>
2022-06-26 00:05:53 +0200

    project started, list page almost done

---
+++ ProjektSemestralny/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSemestralny
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string CarName { get; set; }
+        public string Engine { get; set; }
+        public string Color { get; set; }
+        public string State { get; set; }
+        public int CarMileage { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ ProjektSemestralny/List.xaml.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+
+namespace ProjektSemestralny
+{
+    /// <summary>
+    /// Interaction logic for List.xaml
+    /// </summary>
+    public partial class List : Page
+    {
+        public List()
+        {
+            InitializeComponent();
+            var data1 = LoadCollectionData();
+            CarGrid.ItemsSource = data1;
+        }
+        private List<Car> LoadCollectionData()
+        {
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+            cars.Add(new Car()
+            {
+                Id = 101,
+                CarName = "Jeep",
+                Engine = "200KM",
+                Color = "red",
+                State = "nowy",
+                CarMileage = 2342342,
+                Year = 2009
+            });
+
+            return cars;
+        }
+
+    }
+}
