7f11544086cfae7102c6bd27d85a231833ec4787
mikord45 <51155742+mikord45@users.noreply.github.com>
2022-06-26 02:25:58 +0200

    all pages done

---
+++ ProjektSemestralny/Edit.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+
+namespace ProjektSemestralny
+{
+    /// <summary>
+    /// Interaction logic for Edit.xaml
+    /// </summary>
+    public partial class Edit : Page
+    {
+        public Edit()
+        {
+            InitializeComponent();
+            InputCarName.Text = "";
+            InputMileage.Text = "";
+            InputYear.Text = "";
+            var data1 = LoadEngines();
+            SelectEngine.DisplayMemberPath = "Name";
+            SelectEngine.SelectedValuePath = "Id";
+            SelectEngine.DataContext = data1;
+            var data2 = LoadColors();
+            SelectColor.DisplayMemberPath = "Name";
+            SelectColor.SelectedValuePath = "Id";
+            SelectColor.DataContext = data2;
+            var data3 = LoadStates();
+            SelectState.DisplayMemberPath = "Name";
+            SelectState.SelectedValuePath = "Id";
+            SelectState.DataContext = data3;
+            var data4 = LoadIds();
+            SelectId.DisplayMemberPath = "Name";
+            SelectId.SelectedValuePath = "ID";
+            SelectId.DataContext = data4;
+        }
+
+        private List<Engine> LoadEngines()
+        {
+            List<Engine> engines = new List<Engine>();
+            engines.Add(new Engine()
+            {
+                Id = 101,
+                Name = "Engine1"
+            });
+            engines.Add(new Engine()
+            {
+                Id = 102,
+                Name = "Engine2"
+            });
+
+            return engines;
+        }
+
+        private List<Color> LoadColors()
+        {
+            List<Color> colors = new List<Color>();
+            colors.Add(new Color()
+            {
+                Id = 201,
+                Name = "Color1"
+            });
+            colors.Add(new Color()
+            {
+                Id = 202,
+                Name = "Color2"
+            });
+
+            return colors;
+        }
+
+        private List<State> LoadStates()
+        {
+            List<State> states = new List<State>();
+            states.Add(new State()
+            {
+                Id = 301,
+                Name = "Nowy"
+            });
+            states.Add(new State()
+            {
+                Id = 302,
+                Name = "Używany"
+            });
+            states.Add(new State()
+            {
+                Id = 303,
+                Name = "Uszkodzony"
+            });
+
+            return states;
+        }
+
+        private List<Id> LoadIds()
+        {
+            List<Id> ids = new List<Id>();
+            ids.Add(new Id()
+            {
+                ID = 401,
+                Name = "401"
+            });
+            ids.Add(new Id()
+            {
+                ID = 402,
+                Name = "402"
+            });
+            ids.Add(new Id()
+            {
+                ID = 403,
+                Name = "403"
+            });
+
+            return ids;
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
---
+++ ProjektSemestralny/Id.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSemestralny
+{
+    public class Id
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
